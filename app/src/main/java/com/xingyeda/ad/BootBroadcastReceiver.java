bc4675031784dc5e22bf5b701a804c5a57604852
80150076@163.com <80150076@163.com>
2019-08-09 15:57:28 +0800

    去掉开机广播

--- app/src/main/java/com/xingyeda/ad/BootBroadcastReceiver.java
+++ app/src/main/java/com/xingyeda/ad/BootBroadcastReceiver.java
@@ -9,10 +9,10 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(ACTION)) {
-            Intent mainActivityIntent = new Intent(context, MainActivity.class);  // 要启动的Activity
-            mainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(mainActivityIntent);
-        }
+//        if (intent.getAction().equals(ACTION)) {
+//            Intent mainActivityIntent = new Intent(context, MainActivity.class);  // 要启动的Activity
+//            mainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            context.startActivity(mainActivityIntent);
+//        }
     }
 }
