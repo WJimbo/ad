1d68d154f75fd3fdac32eb1031654b88fa87f9a6
tangyongx <80150076@163.com>
2020-03-19 16:26:13 +0800

    重构广告工程

--- app/src/main/java/com/xingyeda/ad/MainApplication.java
+++ app/src/main/java/com/xingyeda/ad/MainApplication.java
@@ -18,10 +18,10 @@ public class MainApplication extends Application {
     public void onCreate() {
         super.onCreate();
         DeviceUUIDManager.setUuidPrefix("ad");
-        if(BuildConfig.DEBUG){
+//        if(BuildConfig.DEBUG){
 //            DeviceUUIDManager.setUUID("6c21a218fb77");
             DeviceUUIDManager.setUUID("73e4a4fda9a9f73c");
-        }
+//        }
         if(ApplicationUtil.isMainProcess(this)) {
             ToolUtils.init(this);
             LoggerHelper.init();
