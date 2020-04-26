8e12590c667c53a1cb4913f94cf66f0bf1b64e59
tangyongx <80150076@163.com>
2020-04-26 20:40:51 +0800

    新接口地址

--- app/src/main/java/com/xingyeda/ad/config/URLConfig.java
+++ app/src/main/java/com/xingyeda/ad/config/URLConfig.java
@@ -11,10 +11,9 @@ public class URLConfig {
 
     public static final String DOMAIN = "http://";
 
-
     //    public static final String HOST = "192.168.10.200";
 //    private static final String HOST = "ytyty.utools.club";//新后台测试地址
-    private static final String HOST = "test.xyd999.net:7070";
+    private static final String HOST = "47.112.252.45:8080";
 //    private static final String HOST = "service.xyd999.net";
 
 
