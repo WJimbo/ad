3f649038942e988e3fede698395acf2dea69d76a
tangyongx <80150076@163.com>
2020-06-12 14:24:18 +0800

    MultiDex和LeakCanary添加

--- app/src/main/java/com/xingyeda/ad/MainApplication.java
+++ app/src/main/java/com/xingyeda/ad/MainApplication.java
@@ -2,9 +2,12 @@ package com.xingyeda.ad;
 
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.lansosdk.videoeditor.LanSoEditor;
 import com.liulishuo.filedownloader.FileDownloader;
+import com.squareup.leakcanary.LeakCanary;
 import com.xingyeda.ad.config.DeviceUUIDManager;
 import com.xingyeda.ad.module.ad.data.DownloadManager;
 import com.xingyeda.ad.util.CrashHandler;
@@ -15,6 +18,11 @@ import com.zz9158.app.common.utils.LoggerHelper;
 import com.zz9158.app.common.utils.ToolUtils;
 
 public class MainApplication extends Application {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
 
     @Override
     public void onCreate() {
@@ -25,7 +33,13 @@ public class MainApplication extends Application {
 //            DeviceUUIDManager.setUUID("c44cede2765a169b");
 //            DeviceUUIDManager.setUUID("bb4abd3a345c8d0b");
 //            DeviceUUIDManager.setUUID("7cc8d66b13fc0063");
+            DeviceUUIDManager.setUUID("dda29da822020797");
         }
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            //此过程专用于LeakCanary进行堆分析。在此过程中不应初始化应用程序。
+            return;
+        }
+        LeakCanary.install(this);
         if(ApplicationUtil.isMainProcess(this)) {
             TokenMananger.getInstance().init(this, DeviceUUIDManager.generateUUID(this),"1");
             DownloadManager.getInstance().setContext(this);
